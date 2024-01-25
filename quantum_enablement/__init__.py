b13ed400b01f82a0aea4aa7640e19df169ccd19e
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2024-01-25 10:18:46 -0500

    docs: update project description

--- quantum_enablement/__init__.py
+++ quantum_enablement/__init__.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2023.
+# (C) Copyright IBM 2024.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -10,9 +10,9 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Qiskit-based Python project."""
+"""IBM Quantum Enablement library."""
 
-__copyright__ = "(C) Copyright IBM 2023"
+__copyright__ = "(C) Copyright IBM 2024"
 __version__ = "0.0.0"
 
 
