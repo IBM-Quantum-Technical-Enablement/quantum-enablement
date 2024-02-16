ffd2e3e468ed95528d17611d6c861fc84cf434d9
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2024-02-15 22:10:00 -0500

    docs(circuits): fix docstring for utils module

--- quantum_enablement/circuits/_utils.py
+++ quantum_enablement/circuits/_utils.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Miscellaneous tools and circuits."""
+"""Utility tools for quantum circuits."""
 
 
 from qiskit import QuantumCircuit
